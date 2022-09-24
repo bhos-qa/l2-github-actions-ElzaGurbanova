d1758b21f524d88fbba6c00667a17e0e4113f5e5
ElzaGurbanova <a1e13ccc@gmail.com>
2022-09-24 10:23:37 +0400

    Second Lab task

---
+++ src/main/java/org/First/Main.java
@@ -0,0 +1,10 @@
+package org.First;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+    public static int returnStringLength(String word) {
+        return word.length();
+    }
+}
